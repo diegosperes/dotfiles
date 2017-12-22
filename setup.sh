882eeba3f35981a105c24326601fde4b68c4c870
Diego da Silva Péres <diego@MacBook-Pro-de-Diego.local>
2017-12-22 14:56:06 -0200

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# Install homebrew 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install bash-completion
+
+ln -s "$PWD/.bash_profile" ~/.bash_profile
+ln -s "$PWD/.vimrc" ~/.vimrc
