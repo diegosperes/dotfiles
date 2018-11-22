f2a1de6524ac9c14508cca718f169f8f99a63829
Diego Peres <speres.diego@gmail.com>
2018-11-22 18:41:46 -0200

    Updating setup to install chrome, iterm, docker, mongodb, redis, and mysql

--- setup.sh
+++ setup.sh
@@ -5,10 +5,13 @@ defaults write NSGlobalDomain KeyRepeat -int 1 # Set the cursor speed to minimum
 # Install homebrew 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-brew install bash-completion
-curl -L "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > "$PATHDOTFILES/bash-functions/git-completion"
-curl -L "https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim" > "$PATHDOTFILES/.vim/colors/solarized.vim"
-
 ln -s "$PATHDOTFILES/.vim/" ~/
 ln -s "$PATHDOTFILES/.bash_profile" ~/.bash_profile
 ln -s "$PATHDOTFILES/.vimrc" ~/.vimrc
+
+source ~/.bash_profile
+
+brew cask install google-chrome iterm2 docker
+brew install bash-completion python@2 python@3 mongodb redis mysql
+curl -L "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" > "$PATHDOTFILES/bash-functions/git-completion"
+curl -L "https://raw.githubusercontent.com/altercation/solarized/master/vim-colors-solarized/colors/solarized.vim" > "$PATHDOTFILES/.vim/colors/solarized.vim"
